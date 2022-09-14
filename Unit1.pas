1e5298822df73600f61d42d32eacabbcbd117ce9
CryptoTangerine <crypto.tangerine@gmail.com>
2022-09-14 17:08:00 +0200

    viewport

--- Unit1.pas
+++ Unit1.pas
@@ -4,10 +4,19 @@ interface
 
 uses
   System.SysUtils, System.Types, System.UITypes, System.Classes, System.Variants,
-  FMX.Types, FMX.Controls, FMX.Forms, FMX.Graphics, FMX.Dialogs;
+  FMX.Types, FMX.Controls, FMX.Forms, FMX.Graphics, FMX.Dialogs, FMX.StdCtrls,
+  FMX.Controls.Presentation, FMX.Colors, FMX.Edit, FMX.EditBox, FMX.SpinBox,
+  FMX.Viewport3D;
 
 type
   TForm1 = class(TForm)
+    Panel1: TPanel;
+    GroupBox1: TGroupBox;
+    SpinBox1: TSpinBox;
+    SpinBox2: TSpinBox;
+    ColorPicker1: TColorPicker;
+    Button1: TButton;
+    Viewport3D1: TViewport3D;
   private
     { Private declarations }
   public
